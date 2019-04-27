9779a1abf3ac737dc93630c136e59f4f66d84483
Michael Carstiars <michael.j.carstairs@gmail.com>
2019-04-27 11:28:54 +0100

    Update 1

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    Animator anim; 
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("Pushed", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("Pushed", false);
+    }
+}
